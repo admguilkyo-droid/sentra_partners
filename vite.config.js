45c50ff4ff335f2b1cfbdf7cf0b379f043f5f279
admguilkyo-droid <admguilkyo@gmail.com>
2025-09-12 22:59:40 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,34 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-{
-"scripts": {
-"dev": "vite",
-"build": "vite build",
-"preview": "vite preview --host --port $PORT"
-}
-}
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-export default defineConfig({
-plugins: [react()],
-resolve: {
-alias: {
-'@': path.resolve(__dirname, './src'),
-},
-},
-server: {
-host: true,
-port: process.env.PORT || 5173
-},
-preview: {
-host: true,
-port: process.env.PORT || 4173
-}
-})
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
